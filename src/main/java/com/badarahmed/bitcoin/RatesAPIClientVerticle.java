02641891bf7dd88357ca43bc943d364b0b88a859
superfeddy <fedf0415@gmail.com>
2016-08-23 16:32:03 -0700

    Get rates from exchange API every 5 secs

--- src/main/java/com/badarahmed/bitcoin/RatesAPIClientVerticle.java
+++ src/main/java/com/badarahmed/bitcoin/RatesAPIClientVerticle.java
@@ -10,26 +10,33 @@ public class RatesAPIClientVerticle extends AbstractVerticle {
     @Override
     public void start(Future<Void> fut) {
         EventBus eb = vertx.eventBus();
+        getCurrentRates(eb);
 
+        vertx.setPeriodic(5000, timerId -> {
+            getCurrentRates(eb);
+        });
+    }
+
+    private void getCurrentRates(EventBus eb) {
         vertx.createHttpClient(new HttpClientOptions().setSsl(true))
                 .getNow(443, "www.okcoin.com", "/api/v1/ticker.do?symbol=btc_usd",
-                response -> {
-                    if (response.statusCode() == 200) {
+                        response -> {
+                            if (response.statusCode() == 200) {
 
-                        response.handler(body -> {
-                            eb.send("rates-client", body.toString(), reply -> {
-                                if (reply.succeeded()) {
-                                    System.out.println("[RatesAPIClientVerticle] Received reply: " + reply.result().body());
-                                } else {
-                                    System.out.println("[RatesAPIClientVerticle] No reply");
-                                }
-                            });
+                                response.handler(body -> {
+                                    eb.send("rates-client", body.toString(), reply -> {
+                                        if (reply.succeeded()) {
+                                            System.out.println("[RatesAPIClientVerticle] Received reply: " + reply.result().body());
+                                        } else {
+                                            System.out.println("[RatesAPIClientVerticle] No reply");
+                                        }
+                                    });
+                                });
+
+                            } else {
+                                System.out.println("[RatesAPIClientVerticle] Bad response from API: " + response.statusCode() + " " + response.statusMessage());
+                            }
                         });
-
-                    } else {
-                        System.out.println("[RatesAPIClientVerticle] Bad response from API: " + response.statusCode() + " " + response.statusMessage());
-                    }
-                });
     }
 
 }
